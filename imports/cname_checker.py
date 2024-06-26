312fe002c780dfb6a8782b5faf43c15a961aa8b0
incendiary <a.horsewood@googlemail.com>
2024-06-26 18:04:29 +0800

    Refactor cname_checker.py for clarity and readability
    The cname_checker.py file has been extensively refactored. Code comments and docstrings were added to improve readability and provide a more comprehensive explanation of the module and its functions. Calls to `open` now specify the file encoding "utf-8" for better internationalization support.

--- imports/cname_checker.py
+++ imports/cname_checker.py
@@ -1,17 +1,47 @@
-import dns.resolver
-import re
-import json
+"""
+This module provides methods to detect
+potential cloud service takeovers by identifying dangling CNAME records.
 
-# Function to detect potential cloud service takeovers
+Functions:
+--------
+detect_direct_takeovers(dangling_cname_file, output_file):
+    Detect potential takeover candidates from a list of dangling CNAME records.
+
+check_dangling_cname(current_domain, nameservers):
+    Check if a domain has a dangling CNAME record.
+"""
+
+import json
+import re
+
+import dns.resolver
 
 
 def detect_direct_takeovers(dangling_cname_file, output_file):
-    with open("config.json") as f:
+    """
+
+    :param dangling_cname_file: Path to the file containing the list of dangling CNAME records.
+    :param output_file: Path to the output file where potential takeover candidates will be written.
+    :return: None
+
+    This method is used to detect potential takeover candidates from a list of dangling
+    CNAME records. It reads a configuration file called "config.json" which contains patterns
+    for different cloud providers. The method then checks each domain in the dangling_cname_file
+    against the patterns and writes potential candidates to the output_file.
+
+    Example usage:
+
+    detect_direct_takeovers("dangling_cname_records.txt", "output.txt")
+
+    """
+    with open("config.json", "r", encoding="utf-8") as f:
         patterns = json.load(f)
 
     patterns = {k: re.compile(v) for k, v in patterns.items()}
 
-    with open(dangling_cname_file, "r") as infile, open(output_file, "a") as outfile:
+    with open(dangling_cname_file, "r", encoding="utf-8") as infile, open(
+        output_file, "a", encoding="utf-8"
+    ) as outfile:
         for line in infile:
             domain = line.strip()
             for cloud_provider, pattern in patterns.items():
@@ -25,6 +55,16 @@ def detect_direct_takeovers(dangling_cname_file, output_file):
 
 
 def check_dangling_cname(current_domain, nameservers):
+    """
+    Check if a domain has a dangling CNAME record
+
+    :param current_domain: the domain to check
+    :type current_domain: str
+    :param nameservers: optional list of nameservers to use for resolution
+    :type nameservers: list[str]
+    :return: True if the domain has a dangling CNAME record, False otherwise
+    :rtype: bool
+    """
     resolver = dns.resolver.Resolver()
     if nameservers:
         resolver.nameservers = nameservers
