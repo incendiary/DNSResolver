adb8014f60a31ad1ce71d706f51fcf3b9906916d
incendiary <a.horsewood@googlemail.com>
2024-06-26 18:16:31 +0800

    Add detailed documentation to environment.py module
    The update includes docstrings for module-level functions in environment.py. This provides a clear understanding of each function's purpose, input parameters, and returns. Error handling for file creation is also improved.

--- imports/environment.py
+++ imports/environment.py
@@ -1,19 +1,46 @@
-import os
-import socket
-import requests
-import json
-import sys
+"""
+This module provides utility functions for retrieving environment information
+and file management.
 
+Functions:
+---------
+- get_environment_info: Gathers and returns information about the current
+environment basis.
+  It returns the command executed, an indication of whether the script runs
+  in a docker environment,
+  and the external IP address of the machine.
+
+- create_empty_files: Takes a dictionary with file names as keys and their
+corresponding paths as values.
+  It uses this information to create empty files at the specified locations.
+
+- create_empty_file: Creates an empty file at the specified filename.
+"""
+
+import sys
+import os
+import requests
 from requests import RequestException
 
 
 def get_environment_info():
+    """
+    Returns information about the current environment.
+
+    :return: a dictionary containing the following information:
+             - command_executed: a string representing the command
+                executed to run the script
+             - external_ip: a string representing the external IP
+                address of the current machine
+             - run_in_docker: a boolean indicating whether the
+                script is running inside a Docker container
+    """
     command_executed = " ".join(sys.argv)
 
     running_in_docker = os.path.exists("/.dockerenv")
 
     try:
-        response = requests.get("https://ifconfig.io/ip")
+        response = requests.get("https://ifconfig.io/ip", timeout=10)
         external_ip = response.text.strip()
     except RequestException as error:
         external_ip = (
@@ -30,10 +57,30 @@ def get_environment_info():
 
 
 def create_empty_files(output_files):
+    """
+    Create empty files.
+
+    :param output_files: a dictionary containing file names as keys
+        and their corresponding paths as values
+    :type output_files: dict
+    :return: None
+    """
     for key, value in output_files.items():
         create_empty_file(value)
 
 
 def create_empty_file(filename):
-    with open(filename, "w") as f:
-        pass
+    """
+    Create an empty file with the given filename.
+
+    :param filename: The name of the file to create.
+    :type filename: str
+    :return: None
+    """
+    if not isinstance(filename, str):
+        raise ValueError("filename must be a string")
+    try:
+        with open(filename, "w", encoding="utf-8") as f:
+            pass
+    except IOError as e:
+        print(f"Unable to create file {filename}. Error: {e}")
