74f3ddc8827a8f5b6dfed2eab1d77d3477484d51
A <shivers.ion.0m@icloud.com>
2024-07-01 14:46:07 +0800

    Refactor domain resolution into separate functions
    Simplified the domain resolution process by moving the DNS resolving code into a separate function create_resolver(), and the domain resolving code into resolve_domain(). This cleanup modularizes the process, making the code easier to read, maintain, and debug.

--- imports/dns_based_checks.py
+++ imports/dns_based_checks.py
@@ -11,6 +11,114 @@ check_dangling_cname(current_domain, nameservers):
 import dns.resolver
 
 
+def create_resolver(timeout, nameservers):
+    resolver = dns.resolver.Resolver()
+    resolver.lifetime = timeout  # Set the timeout for the resolver
+    resolver.timeout = timeout  # Set the timeout for individual requests
+    if nameservers:
+        resolver.nameservers = nameservers
+
+    return resolver
+
+
+def resolve_domain(resolver, domain, nameservers, output_files, verbose):
+
+    resolved_records = []
+
+    try:
+        current_domain = domain
+        while True:
+            cname_chain_resolved = False
+            for record_type in ["CNAME"]:
+                try:
+                    answer = resolver.resolve(current_domain, record_type)
+                    resolved_records.append(
+                        (record_type, [str(rdata) for rdata in answer])
+                    )
+                    current_domain = str(answer[0].target)
+                    cname_chain_resolved = True
+                    # Check if the domain is a dangling cname
+                    if check_dangling_cname(
+                        current_domain, nameservers, domain, output_files
+                    ):
+                        # Write the domain to output file
+                        with open(
+                            output_files["standard"]["dangling"], "a", encoding="utf-8"
+                        ) as file:
+                            print(
+                                f"Writing resolved records for domain: {domain}|{current_domain} to "
+                                f"{output_files['standard']['dangling']}"
+                            )
+                            file.write(f"{domain}|{current_domain}\n")
+                    break
+                except (
+                    dns.resolver.NoAnswer,
+                    dns.resolver.NXDOMAIN,
+                    dns.resolver.NoNameservers,
+                    dns.resolver.Timeout,
+                ):
+                    continue
+
+            if not cname_chain_resolved:
+                break
+
+        # Resolve A and AAAA records for the final domain
+        final_ips = []
+        for record_type in ["A", "AAAA"]:
+            try:
+                answer = resolver.resolve(current_domain, record_type)
+                final_ips.extend([str(rdata) for rdata in answer])
+                resolved_records.append((record_type, [str(rdata) for rdata in answer]))
+            except (
+                dns.resolver.NoAnswer,
+                dns.resolver.NXDOMAIN,
+                dns.resolver.NoNameservers,
+                dns.resolver.Timeout,
+            ):
+                continue
+
+        if resolved_records:
+            if verbose:
+                print(
+                    f"Writing resolved records for domain: {domain} to {output_files['standard']['resolved']}"
+                )
+
+            with open(output_files["standard"]["resolved"], "a", encoding="utf-8") as f:
+                f.write(f"{domain}:\n")
+                for record_type, records in resolved_records:
+                    f.write(f"  {record_type}:\n")
+                    for record in records:
+                        f.write(f"    {record}\n")
+                f.write("--------\n")
+
+            return True, final_ips
+
+    except dns.exception.Timeout as e:
+        # Handle the timeout exception by writing to the timeout file
+        with open(output_files["standard"]["timeout"], "a", encoding="utf-8") as f:
+            f.write(f"{domain}\n")
+            f.write("--------\n")
+
+        if verbose:
+            print(f"DNS resolution for {domain} timed out: {e}")
+
+        return False
+
+    except dns.exception.DNSException as e:
+        with open(output_files["standard"]["unresolved"], "a", encoding="utf-8") as f:
+            f.write(f"{domain}:\n")
+            for record_type, records in resolved_records:
+                f.write(f"  {record_type}:\n")
+                for record in records:
+                    f.write(f"    {record}\n")
+            f.write("--------\n")
+
+        if verbose:
+            print(f"Failed to resolve {domain}: {e}")
+
+        return False
+
+
 def check_dangling_cname(current_domain, nameservers, original_domain, output_files):
     """
     Check if a domain has a dangling CNAME record
@@ -55,7 +163,7 @@ def check_dangling_cname(current_domain, nameservers, original_domain, output_fi
                         output_files["standard"]["ns_takeover"], "a", encoding="utf-8"
                     ) as file:
                         file.write(f"{original_domain}|{current_domain}\n")
-                    return False  # If resolving the NS record succeeds, log and it's not a dangling CNAME
+                    return False  # If resolving the NS record succeeds, log, and it's not a dangling CNAME
                 except (
                     dns.resolver.NoAnswer,
                     dns.resolver.NXDOMAIN,
--- imports/domain_processor.py
+++ imports/domain_processor.py
@@ -19,7 +19,7 @@ optionally be printed to the console in verbose scenarios.
 
 import dns.resolver
 from imports.cloud_csp_checks import perform_csp_checks
-from imports.dns_based_checks import check_dangling_cname
+from imports.dns_based_checks import resolve_domain, create_resolver
 from imports.service_connectivity_checks import perform_service_connectivity_checks
 
 
@@ -59,119 +59,30 @@ def process_domain(
     :return: None
     """
 
-    resolver = dns.resolver.Resolver()
-    resolver.lifetime = timeout  # Set the timeout for the resolver
-    resolver.timeout = timeout  # Set the timeout for individual requests
-    if nameservers:
-        resolver.nameservers = nameservers
+    resolver = create_resolver(timeout, nameservers)
 
-    resolved_records = []
+    print(verbose)
 
-    try:
-        current_domain = domain
-        while True:
-            cname_chain_resolved = False
-            for record_type in ["CNAME"]:
-                try:
-                    answer = resolver.resolve(current_domain, record_type)
-                    resolved_records.append(
-                        (record_type, [str(rdata) for rdata in answer])
-                    )
-                    current_domain = str(answer[0].target)
-                    cname_chain_resolved = True
-                    # Check if the domain is a dangling cname
-                    if check_dangling_cname(
-                        current_domain, nameservers, domain, output_files
-                    ):
-                        # Write the domain to output file
-                        with open(
-                            output_files["standard"]["dangling"], "a", encoding="utf-8"
-                        ) as file:
-                            print(
-                                f"Writing resolved records for domain: {domain}|{current_domain} to "
-                                f"{output_files['standard']['dangling']}"
-                            )
-                            file.write(f"{domain}|{current_domain}\n")
-                    break
-                except (
-                    dns.resolver.NoAnswer,
-                    dns.resolver.NXDOMAIN,
-                    dns.resolver.NoNameservers,
-                    dns.resolver.Timeout,
-                ):
-                    continue
+    success, final_ips = resolve_domain(
+        resolver, domain, nameservers, output_files, verbose
+    )
+    # returns true if domain completed resolution
+    if success:
+        perform_csp_checks(
+            domain,
+            output_files,
+            final_ips,
+            gcp_ipv4,
+            gcp_ipv6,
+            aws_ipv4,
+            aws_ipv6,
+            azure_ipv4,
+            azure_ipv6,
+            verbose,
+            extreme,
+        )
 
-            if not cname_chain_resolved:
-                break
+        if perform_service_checks:
+            perform_service_connectivity_checks(domain, output_files, verbose, extreme)
 
-        # Resolve A and AAAA records for the final domain
-        final_ips = []
-        for record_type in ["A", "AAAA"]:
-            try:
-                answer = resolver.resolve(current_domain, record_type)
-                final_ips.extend([str(rdata) for rdata in answer])
-                resolved_records.append((record_type, [str(rdata) for rdata in answer]))
-            except (
-                dns.resolver.NoAnswer,
-                dns.resolver.NXDOMAIN,
-                dns.resolver.NoNameservers,
-                dns.resolver.Timeout,
-            ):
-                continue
-
-        if resolved_records:
-            if verbose:
-                print(
-                    f"Writing resolved records for domain: {domain} to {output_files['standard']['resolved']}"
-                )
-
-            with open(output_files["standard"]["resolved"], "a", encoding="utf-8") as f:
-                f.write(f"{domain}:\n")
-                for record_type, records in resolved_records:
-                    f.write(f"  {record_type}:\n")
-                    for record in records:
-                        f.write(f"    {record}\n")
-                f.write("--------\n")
-
-            perform_csp_checks(
-                domain,
-                output_files,
-                final_ips,
-                gcp_ipv4,
-                gcp_ipv6,
-                aws_ipv4,
-                aws_ipv6,
-                azure_ipv4,
-                azure_ipv6,
-                verbose,
-                extreme,
-            )
-
-            if perform_service_checks:
-                perform_service_connectivity_checks(
-                    domain, output_files, verbose, extreme
-                )
-
-    except dns.exception.Timeout as e:
-        # Handle the timeout exception by writing to the timeout file
-        with open(output_files["standard"]["timeout"], "a", encoding="utf-8") as f:
-            f.write(f"{domain}\n")
-            f.write("--------\n")
-
-        if verbose:
-            print(f"DNS resolution for {domain} timed out: {e}")
-
-    except dns.exception.DNSException as e:
-        with open(output_files["standard"]["unresolved"], "a", encoding="utf-8") as f:
-            f.write(f"{domain}:\n")
-            for record_type, records in resolved_records:
-                f.write(f"  {record_type}:\n")
-                for record in records:
-                    f.write(f"    {record}\n")
-            f.write("--------\n")
-
-        if verbose:
-            print(f"Failed to resolve {domain}: {e}")
-
-    finally:
-        pbar.update(1)
+    pbar.update(1)
